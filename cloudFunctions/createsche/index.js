c00786c9a1ae667917e5a6fd856eeb57e969d60d
Cheryl <1030821728@qq.com>
2020-04-21 21:05:03 +0800

    前端测试，创建schedule函数（未跑通）

---
+++ cloudFunctions/createsche/index.js
@@ -0,0 +1,19 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+})
+// const db = cloud.database();
+// const userCollection = db.collection("Schedules");
+
+
+// 云函数入口函数
+exports.main = async event=> {
+  const wxContext = cloud.getWXContext()
+  console.log("!")
+  const { title, description } = event;
+  console.log('event', event);
+  return {
+    title,description
+  }
+}
