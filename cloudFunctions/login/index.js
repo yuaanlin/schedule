e2f98949239af9a051314c16e9c40fbec516f4f0
Yuanlin Lin <im.yuanlinlin@gmail.com>
2020-04-28 20:32:18 +0800

    把 openId 栏位改名为 _id 并修改 return 格式

--- cloudFunctions/login/index.js
+++ cloudFunctions/login/index.js
@@ -1,42 +1,33 @@
-const cloud = require('wx-server-sdk')
-cloud.init()
-const db = cloud.database()
-const userCollection = db.collection('User')
+const cloud = require("wx-server-sdk");
+cloud.init();
+const db = cloud.database();
+const userCollection = db.collection("User");
 
 exports.main = async () => {
-  const { OPENID } = cloud.getWXContext()
-  console.log(OPENID)
+    const { OPENID } = cloud.getWXContext();
 
-  try {
-    const allUser = (await userCollection.get()).data
-    const [userInfo] = allUser.filter(v => v.openId === OPENID)
-    console.log('查到的userInfo', userInfo)
-    let name, avatarUrl, gender
-    // 无记录，加记录
-    if (!userInfo) {
-      await userCollection.add({
-        data: {
-          openId: OPENID,
-          createdTime: db.serverDate(),
-        },
-      })
-      // 有记录，返回用户信息
-    } else {
-      name = userInfo.name
-      avatarUrl = userInfo.avatarUrl
-      gender = userInfo.gender
+    try {
+        const allUser = (await userCollection.get()).data;
+        const [userInfo] = allUser.filter(v => v._id === OPENID);
+        let name, avatarUrl, gender;
+        // 无记录，请前端调用 postUserInfo 来添加记录
+        if (!userInfo) return { code: 404 };
+        // 有记录，回传记录
+        else
+            return {
+                code: 200,
+                data: {
+                    name: userInfo.name,
+                    avatarUrl: userInfo.avatarUrl,
+                    gender: userInfo.gender,
+                    openId: OPENID
+                }
+            };
+    } catch (e) {
+        console.error(e);
+        return {
+            code: 500,
+            message: "服务器错误"
+        };
     }
-    return {
-      name: name || null,
-      avatarUrl: avatarUrl || null,
-      gender: gender || null,
-      openId: OPENID,
-    }
-  } catch (e) {
-    console.error(e)
-    return {
-      code: 500,
-      message: '服务器错误',
-    }
-  }
-}
+};
--- cloudFunctions/postUserInfo/index.js
+++ cloudFunctions/postUserInfo/index.js
@@ -4,42 +4,53 @@ const db = cloud.database();
 const userCollection = db.collection("User");
 
 exports.main = async event => {
-  const { OPENID } = cloud.getWXContext();
-  const { name, gender, avatarUrl } = event;
-  console.log('event', event);
+    const { OPENID } = cloud.getWXContext();
+    const { name, gender, avatarUrl } = event;
 
-  try {
-    const [userRecord] = (await userCollection
-      .where({
-        openId: OPENID
-      })
-      .get()).data;
-    console.log("查到的用户信息", userRecord);
-    if (!userRecord) {
-      return {
-        code: 1,
-        message: "用户不存在"
-      };
-    } else {
-      await userCollection.doc(userRecord._id).update({
-        data: {
-          name,
-          gender,
-          avatarUrl
+    try {
+        const [userRecord] = (
+            await userCollection
+                .where({
+                    _id: OPENID
+                })
+                .get()
+        ).data;
+
+        if (!userRecord) {
+            // 无记录，加记录
+            await userCollection.add({
+                data: {
+                    _id: OPENID,
+                    createdTime: db.serverDate(),
+                    name,
+                    gender,
+                    avatarUrl
+                }
+            });
+        } else {
+            // 有记录，更新记录
+            await userCollection.doc(userRecord._id).update({
+                data: {
+                    name,
+                    gender,
+                    avatarUrl
+                }
+            });
         }
-      });
+        return {
+            code: 200,
+            data: {
+                openId: OPENID,
+                name,
+                gender,
+                avatarUrl
+            }
+        };
+    } catch (e) {
+        console.log(e);
+        return {
+            code: 500,
+            message: "服务器错误"
+        };
     }
-    return {
-      openId: OPENID,
-      name,
-      gender,
-      avatarUrl
-    };
-  } catch (e) {
-    console.log(e);
-    return {
-      code: 500,
-      message: "服务器错误",
-    }
-  }
 };
