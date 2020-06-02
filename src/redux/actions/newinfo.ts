5942715a55d3b51998663687eadafc87d8981a6f
Yuanlin Lin <im.yuanlinlin@gmail.com>
2020-06-02 19:01:46 +0800

    删除无用注释

--- src/redux/actions/newinfo.ts
+++ src/redux/actions/newinfo.ts
@@ -6,21 +6,10 @@ interface updatenewInfo {
     data: newinfo;
 }
 
-// interface deleteInfo {
-//     type: typeof DELETE_INFO;
-//     data: string;
-// }
-
 /** 对 Redux 中 info 数据的 Actions */
-export type NewinfoActions = updatenewInfo ;
+export type NewinfoActions = updatenewInfo;
 
 /** 将新的 newInfo 存入 Redux （或是覆写原有的 newInfo) */
 export function updatenewInfo(data: newinfo): updatenewInfo {
-  console.log(data)
     return { type: UPDATE_NEWINFO, data: data };
 }
-
-/** 从 Redux 删除 Info */
-// export function deleteInfo(data: string): deleteInfo {
-//     return { type: DELETE_INFO, data: data };
-// }
